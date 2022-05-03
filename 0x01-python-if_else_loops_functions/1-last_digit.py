95f7fb9190443faf433611f9f27e6b27cac97a5f
Santiago23z <98781406+Santiago23z@users.noreply.github.com>
2022-05-03 13:13:46 -0500

    new

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,8 @@ if (number < 0):
 else:
     u = number % 10
 if (u > 5):
-    print(f" {str} {number} is {u} and is greater than 5")
+    print(f"{str} {number} is {u} and is greater than 5")
 elif (u == 0):
-    print(f" {str} {number} is {u} and is 0")
+    print(f"{str} {number} is {u} and is 0")
 elif (u < 6 and u != 0):
-    print(f" {str} {number} is {u} and is less than 6 and not 0")
+    print(f"{str} {number} is {u} and is less than 6 and not 0")
