1b87ace8d711cf3d8b8f4c6412ad777d5d6a5930
Santiago23z <98781406+Santiago23z@users.noreply.github.com>
2022-05-05 19:03:11 -0500

    Task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-from sys import argv
-
-def main():
+if __name__ == "__main__":
+    from sys import argv
     result = 0
     if (len(argv) == 1):
         result = 0
@@ -9,6 +8,3 @@ def main():
         for i in argv[1:]:
             result += int(i)
         print(result)
-        
-if __name__ == "__main__":
-    main()
