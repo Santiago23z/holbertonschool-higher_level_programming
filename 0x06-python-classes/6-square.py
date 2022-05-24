e9c9a596e871262c268936210b76a18a8e0117ee
santiago23z <santizapataalvares@gmail.com>
2022-05-24 18:58:18 -0500

    Task 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""define a sqaure class"""
+
+
+class Square:
+    """A simple class without methods or public attributes"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """instantion safe of an square"""
+        self.__size = size
+        self.__position = position
+
+    @property
+    def position(self):
+        """int: return the size of the square"""
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        """sets the attribute size"""
+        self.__position = None
+        if len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value) is not tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[0] is not int or type(value[1]) is not int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    @property
+    def size(self):
+        """int: return the size of the square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """sets the attribute size"""
+        if type(value) == int:
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """int: return the area of the square"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """prints the square with the char '#'"""
+        if self.__size != 0:
+            for i in range(self.__position[1]):
+                print("")
+            for j in range(self.__size):
+                print(f"{' ' * self.__position[0]}{'#' * self.__size}")
+        else:
+            print("")
+            return
