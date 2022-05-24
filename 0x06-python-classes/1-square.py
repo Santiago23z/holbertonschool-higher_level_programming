7e22675e7fde452a5dd69bca07eede497e0dbbe2
santiago23z <santizapataalvares@gmail.com>
2022-05-24 16:49:54 -0500

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define a square class"""
+
+
+class Square:
+    """Represents the atributtes
+    of the square
+    size (int): size
+    """
+    def init(self, size):
+        self.size = size
