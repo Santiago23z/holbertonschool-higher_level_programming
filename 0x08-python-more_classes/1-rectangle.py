c4dd22398d7ea3f7bc999204d6a5ea7ed598aa19
santiago23z <santizapataalvares@gmail.com>
2022-05-31 19:52:28 -0500

    x

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-"""A rectangle with width and height."""
+"""Defines a Rectangle"""
 
 
 class Rectangle:
-    """Rectangle functions and data"""
-
+    """new object of class Square"""
     def __init__(self, width=0, height=0):
-        """ Instantions"""
+        """Initialize Rectangle"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """
-        Getter for width
-        """
         return self.__width
 
     @width.setter
     def width(self, value):
-        
+
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -34,7 +30,7 @@ class Rectangle:
     def height(self, value):
 
         if type(value) is not int:
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
