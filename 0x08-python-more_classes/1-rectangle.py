1aa357275e586ab697a991fa626a79d03c279299
santiago23z <santizapataalvares@gmail.com>
2022-05-31 19:49:39 -0500

    x

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,14 +11,14 @@ class Rectangle:
         """ Instantions"""
         self.width = width
         self.height = height
-    
+
     @property
     def width(self):
         """
         Getter for width
         """
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         """ setter for width
@@ -28,13 +28,13 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    
+
     @property
     def height(self):
         """
         Getter for height """
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         """ setter for height
