2838ce168bba8000fba78c554a00f29c14155923
Santiago23z <santizapataalvares@gmail.com>
2022-06-08 16:33:39 -0500

    Task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import json
+"""
+contains Object to a text file, using a JSON 
+"""
+
+
+def save_to_json_file(my_obj, filename):
+    """function open"""
+    with open(filename, mode="w") as f:
+        json.dump(my_obj, f)
