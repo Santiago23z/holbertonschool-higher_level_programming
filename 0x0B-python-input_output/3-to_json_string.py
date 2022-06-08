8dec5be42391173551ec081a3b69109443faeffd
Santiago23z <santizapataalvares@gmail.com>
2022-06-08 17:32:58 -0500

    Task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
+"""contains json functions import"""
 from json import dumps
-"""
-contains json functions import
-"""
 
 
 def to_json_string(my_obj):
-    """Write a function that returns the JSON 
-    representation of an object (string):"""
+    """Write a function that returns the JSON"""
     return dumps(my_obj)
