1ae4ca6a2d4d3d2114ceb2cb8f8bcb2b5714b341
Santiago23z <santizapataalvares@gmail.com>
2022-06-08 16:05:25 -0500

    Task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import json
+"""
+contains json string
+"""
+
+
+def from_json_string(my_str):
+    """Function load that convert a data type(python) to json string"""
+    return json.loads(my_str)
