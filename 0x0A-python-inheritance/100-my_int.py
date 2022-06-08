5c397a50e38e3c53336bc98f9fdc82aad940f3f1
Santiago23z <santizapataalvares@gmail.com>
2022-06-07 19:59:20 -0500

    Task 100

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,7 +5,7 @@ contains the class myint
 
 
 class MyInt(int):
-    """rebel versionof an integer"""
+    """rebel versionof an integer, perfect for opposite"""
     def __new__(cls, *args, **kwargs):
         """create a new instance of the class"""
         return super(MyInt, cls).__new__(cls, *args, **kwargs)
@@ -13,7 +13,7 @@ class MyInt(int):
     def __eq__(self, other):
         """what was != is now =="""
         return int(self) != other
-    
+
     def __ne__(self, other):
         """what was == is now !="""
         return int(self) == other
