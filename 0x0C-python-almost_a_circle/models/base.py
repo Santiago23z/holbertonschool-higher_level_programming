0dc8f2228f831e825b15ce2d8e9b9f71026ba87e
Santiago23z <santizapataalvares@gmail.com>
2022-06-14 17:23:58 -0500

    Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,7 +27,7 @@ class Base:
         """
         ...
         """
-        if list_dictionaries is None or list_dictionaries == []:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
