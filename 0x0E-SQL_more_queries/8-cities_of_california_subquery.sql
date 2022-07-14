0f35a47a33c665ab645f260304b17f42dd081df8
Santiago23z <santizapataalvares@gmail.com>
2022-07-14 17:04:45 -0500

    Task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,5 @@
---Write a script that lists all the cities of California that can be found in the database hbtn_0d_usa.
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE NOT NULL AUTO_INCREMENT, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, PRIMARY KEY (id));
+--  lists all the cities of California that can be found in the database hbtn_0d_usa.
+-- The states table contains only one record where name = California (but the id can be different, as per the example)
+-- Results must be sorted in ascending order by cities.id.
+
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
