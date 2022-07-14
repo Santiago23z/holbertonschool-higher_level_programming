9b7d6a85ea7ca5e988cb6f763d2abf4c1dd6fd79
Santiago23z <santizapataalvares@gmail.com>
2022-07-14 17:11:43 -0500

    Update

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,5 @@
---Import the database dump from hbtn_0d_tvshows to your MySQL server:
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_shows_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id;
+-- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id.
+
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
