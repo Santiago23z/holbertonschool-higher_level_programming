4b10a2a8fd067bd955134a33410d6c3eec8ecbf0
Santiago23z <santizapataalvares@gmail.com>
2022-08-15 15:05:02 -0500

    Task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,7 +4,7 @@ function valueParams () {
   if (process.argv[2]) {
     console.log(process.argv[2]);
   } else {
-    console.log('No arguments');
+    console.log('No argument');
   }
 }
 valueParams();
