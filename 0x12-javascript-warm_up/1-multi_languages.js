44792dd69a96ab820d4c55088b2c89b5f9284edc
Santiago23z <santizapataalvares@gmail.com>
2022-08-15 13:03:40 -0500

    Task1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const theFirst = 'C is fun';
+const theSecond = 'Python is cool';
+const theThird = 'JavaScript is amazing';
+
+function variables () {
+  console.log(theFirst);
+  console.log(theSecond);
+  console.log(theThird);
+}
+variables();
