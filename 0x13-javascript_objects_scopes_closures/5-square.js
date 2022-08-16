b93868d4c51527a03345c0269958194e1f8c7d9a
Santiago23z <santizapataalvares@gmail.com>
2022-08-16 15:47:12 -0500

    Task 5

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle.js');
+import Rectangle from './4-rectangle.js';
 
-module.exports = class Square extends Rectangle {
+export default class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
