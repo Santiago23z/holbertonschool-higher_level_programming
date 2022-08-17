6ffe6bd2a7851deca3abb102be70805de151be59
Santiago23z <santizapataalvares@gmail.com>
2022-08-16 21:31:30 -0500

    Task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-    const reversed = [];
-    for (let x = 0; x < list.length; x++) reversed.unshift(list[x]);
-    return reversed;
-  };
+  const reversed = [];
+  for (let x = 0; x < list.length; x++) reversed.unshift(list[x]);
+  return reversed;
+};
