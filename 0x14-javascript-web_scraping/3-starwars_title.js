9f49989d857b6a916d227dad90b29bcaf97bc375
Santiago23z <santizapataalvares@gmail.com>
2022-08-17 21:14:30 -0500

    tASKS

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,8 @@
 #!/usr/bin/node
+
 const request = require('request');
-let url = 'http://swapi.co/api/films/';
-let episode = process.argv[2];
-request(url + episode, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    let jsonobj = JSON.parse(body);
-    console.log(jsonobj.title);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
-  }
+const URL = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+request(URL, function (err, response, body) {
+  if (err) console.log(err);
+  console.log(JSON.parse(body).title);
 });
