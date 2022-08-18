a2c2df9d0644afaad97919a7766c9c4412ebd83a
Santiago23z <santizapataalvares@gmail.com>
2022-08-17 21:15:28 -0500

    tASKS

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,22 +1,14 @@
-!/usr/bin/node
+#!/usr/bin/node
+
 const request = require('request');
-const url = process.argv[2];
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    let films = JSON.parse(body).results;
-    let count = 0;
-    for (let filmIndex in films) {
-      let filmChars = films[filmIndex].characters;
-      for (let charIndex in filmChars) {
-        if (filmChars[charIndex].includes('18')) {
-          count++;
-        }
-      }
+const URL = process.argv[2];
+request(URL, function (err, response, body) {
+  if (err) console.log(err);
+  let count = 0;
+  for (const film of JSON.parse(body).results) {
+    for (const character of film.characters) {
+      count += (character.includes('/18/')) ? 1 : 0;
     }
-    console.log(count);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
   }
+  console.log(count);
 });
