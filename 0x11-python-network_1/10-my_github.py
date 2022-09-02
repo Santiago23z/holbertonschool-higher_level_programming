3d2da6330e82eef7041918f25de44ff7f12551c2
Santiago23z <santizapataalvares@gmail.com>
2022-09-02 10:06:32 -0500

    tasks

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """
-takes your Github credentials (username and password) and uses the Github API
-to display your id
+Module takes an Github credentials (username and password)
+and uses the Github API to display the user id
 """
+import requests
+from sys import argv
+
 if __name__ == '__main__':
-    import requests
-    from requests.auth import HTTPBasicAuth
-    from sys import argv
-    r = requests.get('https://api.github.com/users/{}'.format(argv[1]),
-                     auth=HTTPBasicAuth(argv[1], argv[2]))
-    print(r.json().get('id'))
+    url = 'https://api.github.com/users/{}'.format(argv[1])
+    headers = {'Authorization': 'token ' + argv[2]}
+    response = requests.get(url, headers=headers)
+    print(response.json().get('id'))
