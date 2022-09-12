e44fa924ea85bfba1f7835672c0ccb8868106876
Santiago23z <98781406+Santiago23z@users.noreply.github.com>
2022-09-12 11:24:28 -0500

    Update 103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-let myObject = {
+const myObject = {
   type: 'object',
   value: 12
 };
 console.log(myObject);
-myObject.incr = function () {
-  this.value++;
-};
+
+myObject.incr = function () { myObject.value += 1; };
+
 myObject.incr();
 console.log(myObject);
 myObject.incr();
